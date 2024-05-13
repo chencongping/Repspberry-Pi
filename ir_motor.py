2f1b4901edee1bde2b083251dd217638613a5522
chencongping <1084337941@qq.com>
2024-05-13 22:29:57 +0800

    Create ir_motor.py
    ir_motor

---
+++ ir_motor.py
@@ -0,0 +1,40 @@
+from machine import Pin
+import time
+from ir import ir
+from machine import Pin, PWM
+
+#配置红外接收引脚
+pin = Pin(5, Pin.IN, Pin.PULL_UP)
+
+speed = 30
+
+#配置红外接收库
+Ir = ir(pin)
+
+while True:
+    #读取遥控器数据
+    value = Ir.Getir()
+    if value != None:
+        speed=value
+
+
+# 初始化电机小风扇
+fan = PWM(Pin(13))
+fan.freq(1000) # 设置频率
+
+# 数值重映射
+def my_map(x, in_min, in_max, out_min, out_max):
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
+# 设置风扇的转速，speed=[0, 100]
+def pwm_motor(speed):
+    if speed > 100 or speed < 0:
+        print('Please enter a limited speed value of 0-100 ')
+        return
+    pulse = my_map(speed, 0, 100, 0, 65535)
+    fan.duty_u16(pulse)
+
+while True:
+    pwm_motor(speed)
+
+
